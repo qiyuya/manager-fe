e38bee6bac90de03f62b327906b0af7ce24fd73b
qiyu <157453032@qq.com>
2022-02-22 15:50:58 +0800

    创建路由

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>欢迎学习</h1>
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+  name:'Home'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>欢迎来到登录界面</h1>
+<Welcome msg="jack"></Welcome>
+<el-button @click="goHome">回首页</el-button>
+</template>
+
+<script>
+import Welcome from './Welcome.vue'
+export default {
+  name:'login',
+  components:{Welcome},
+  methods:{
+    goHome(){
+      this.$router.push('/welcome')
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
