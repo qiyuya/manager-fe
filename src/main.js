835ac77508e6387549352eec4e27b3d025496fd9
qiyu <157453032@qq.com>
2022-02-24 17:08:24 +0800

    登录界面及交互开发

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import request from './utils/request'
 import storage from './utils/storage'
+import api from './api'
+import store from './store'
 
 console.log('环境变量=',import.meta.env)
 
 const app = createApp(App);
 // 全局挂载
 app.config.globalProperties.$request = request
+app.config.globalProperties.$api = api
 app.config.globalProperties.$storage = storage
-app.use(router).use(ElementPlus).mount('#app')
+app.use(router).use(store).use(ElementPlus).mount('#app')
 
