13b4d1cca15460176f21d844187d2741ef9044fc
qiyu <157453032@qq.com>
2022-02-27 22:15:06 +0800

    实现用户列表管理、删除、新增等交互

---
+++ src/views/User.vue
@@ -0,0 +1,335 @@
+<template>
+  <div class="user-manage">
+    <div class="query-form">
+      <el-form ref="form" :inline="true" :model="user">
+        <el-form-item label="用户Id" prop="userId">
+          <el-input v-model="user.userId" placeholder="请输入用户Id" />
+        </el-form-item>
+        <el-form-item label="用户名称" prop="userName">
+          <el-input v-model="user.userName" placeholder="请输入用户名称" />
+        </el-form-item>
+        <el-form-item label="状态" prop="state">
+          <el-select v-model="user.state">
+            <el-option :value="0" label="所有"></el-option>
+            <el-option :value="1" label="在职"></el-option>
+            <el-option :value="2" label="离职"></el-option>
+            <el-option :value="3" label="试用期"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleQuery">查询</el-button>
+          <el-button @click="handleReset('form')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="base-table">
+      <div class="action">
+        <el-button type="primary" @click="handleCreate">新增</el-button>
+        <el-button type="danger" @click="handlePatchDel">批量删除</el-button>
+      </div>
+      <el-table :data="userList" @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55" />
+        <el-table-column
+          v-for="item in columns"
+          :key="item.prop"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+          :formatter="item.formatter"
+        ></el-table-column>
+        <el-table-column label="操作" width="150">
+          <template #default="scope">
+            <el-button @click="handleClick(scope.row)" size="mini">编辑</el-button>
+            <el-button type="danger" size="mini" @click="handleDel(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        class="pagination"
+        background
+        layout="prev, pager, next"
+        :total="pager.total"
+        :page-size="pager.pageSize"
+        @current-change="handleCurrentChange"
+      ></el-pagination>
+    </div>
+    <el-dialog title="用户新增" v-model="showModal">
+      <el-form ref="dialogFrom" :model="userForm" label-width="100px" :rules="rules">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="userForm.userName" placeholder="请输入用户名称" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="userEmail">
+          <el-input v-model="userForm.userEmail" placeholder="请输入用户邮箱">
+            <template #append>@qiyuya.com</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="userForm.mobile" placeholder="请输入手机号" />
+        </el-form-item>
+        <el-form-item label="岗位" prop="job">
+          <el-input v-model="userForm.job" placeholder="请输入岗位" />
+        </el-form-item>
+        <el-form-item label="状态" prop="state">
+          <el-select v-model="userForm.state">
+            <el-option :value="1" label="在职"></el-option>
+            <el-option :value="2" label="离职"></el-option>
+            <el-option :value="3" label="试用期"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="系统角色" prop="roleList">
+          <el-select
+            v-model="userForm.roleList"
+            placeholder="请选择用户系统角色"
+            multiple
+            style="width: 100%"
+          >
+            <el-option
+              v-for="role in roleList"
+              :key="role._id"
+              :label="role.roleName"
+              :value="role._id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="部门" prop="deptId">
+          <el-cascader
+            v-model="userForm.deptId"
+            placeholder="请选择所属部门"
+            :options="deptList"
+            :props="{ checkStrictly: true, value: '_id', label: 'deptName' }"
+            clearable
+            style="width: 100%"
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="handleClose">取 消</el-button>
+          <el-button type="primary" @click="handleSubmit">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+<script>
+import { getCurrentInstance, onMounted, reactive, ref } from 'vue'
+import api from '../api'
+export default {
+  name: 'user',
+  setup() {
+    const { ctx } = getCurrentInstance()
+    // 获取this
+    const instance = getCurrentInstance()
+    const _this = instance.appContext.config.globalProperties
+    // 初始化用户表单对象
+    const user = reactive({
+      state: 1
+    })
+    // 初始化用户列表数据
+    const userList = ref([])
+    // 初始化分页对象
+    const pager = reactive({
+      pageNum: 1,
+      pageSize: 10
+    })
+    // 选中用户列表对象
+    const checkedUserIds = ref([])
+    // 弹框显示对象
+    const showModal = ref(false)
+    // 新增用户Form对象
+    const userForm = reactive({
+      state: 3
+    })
+    // 所有角色列表
+    const roleList = ref([])
+    // 所有部门列表
+    const deptList = ref([])
+    // 定义表单校验规则
+    const rules = reactive({
+      userName: [
+        {
+          required: true,
+          message: '请输入用户名称',
+          trigger: 'blur'
+        }
+      ],
+      userEmail: [
+        {
+          required: true,
+          message: '请输入用户邮箱',
+          trigger: 'blur'
+        }
+      ],
+      mobile: [
+        {
+          pattern: /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
+          ,
+          message: '请输入正确的手机号格式',
+          trigger: 'blur'
+        }
+      ],
+      deptId: [
+        {
+          required: true,
+          message: '请输入部门',
+          trigger: 'blur'
+        }
+      ],
+    })
+    // 定义动态表格-格式
+    const columns = reactive([
+      {
+        label: '用户ID',
+        prop: 'userId'
+      },
+      {
+        label: '用户名',
+        prop: 'userName'
+      },
+      {
+        label: '用户邮箱',
+        prop: 'userEmail'
+      },
+      {
+        label: '用户角色',
+        prop: 'role',
+        formatter(row, column, value) {
+          return {
+            0: '管理员',
+            1: '普通用户'
+          }[value]
+        }
+      },
+      {
+        label: '用户状态',
+        prop: 'state',
+        formatter(row, column, value) {
+          return {
+            1: '在职',
+            2: '离职',
+            3: '试用期'
+          }[value]
+        }
+      },
+      {
+        label: '注册时间',
+        prop: 'createTime'
+      },
+      {
+        label: '最后登录时间',
+        prop: 'lastLoginTime'
+      },
+    ])
+    // 初始化接口调用
+    onMounted(() => {
+      getUserList()
+      getDeptList()
+      getRoleList()
+    })
+    // 获取用户列表
+    const getUserList = async () => {
+      let params = { ...user, ...pager }
+      try {
+        const { list, page } = await api.getUserList(params)
+        userList.value = list
+        pager.total = page.total
+      } catch (error) { }
+    }
+    // 查询事件,获取用户列表
+    const handleQuery = () => {
+      getUserList()
+    }
+    // 重置查询表单
+    const handleReset = (form) => {
+      ctx.$refs[form].resetFields()
+    }
+    // 分页事件处理
+    const handleCurrentChange = (current) => {
+      pager.pageNum = current
+      getUserList()
+    }
+    // 用户单个删除
+    const handleDel = async (row) => {
+      await api.userDel({
+        userIds: [row.userId] // 可单个删除,也可以批量删除
+      })
+      _this.$message.success('删除成功')
+      getUserList()
+    }
+    // 批量删除
+    const handlePatchDel = async () => {
+      console.log(checkedUserIds.value.length)
+      if (checkedUserIds.value.length == 0) {
+        _this.$message.error('请选择要删除的用户')
+        return
+      }
+      const res = await api.userDel({
+        userIds: checkedUserIds.value // 批量删除
+      })
+      if (res.nModified > 0) {
+        _this.$message.success('删除成功')
+        getUserList()
+      } else {
+        _this.$message.error('修改失败')
+      }
+    }
+    // 表格多选
+    const handleSelectionChange = (list) => {
+      let arr = []
+      list.map(item => {
+        arr.push(item.userId)
+      })
+      checkedUserIds.value = arr
+    }
+    // 用戶新增
+    const handleCreate = () => {
+      showModal.value = true
+    }
+    // 部门列表查询
+    const getDeptList = async () => {
+      let list = await api.getDeptList()
+      deptList.value = list
+    }
+    // 角色列表查询
+    const getRoleList = async () => {
+      let list = await api.getRoleList()
+      roleList.value = list
+    }
+    // 用户弹窗关闭
+    const handleClose = () => {
+      showModal.value = false
+      handleReset('dialogFrom')
+    }
+    // 用户提交
+    const handleSubmit = () => {
+
+    }
+    return {
+      user,
+      userList,
+      columns,
+      pager,
+      checkedUserIds,
+      showModal,
+      userForm,
+      rules,
+      roleList,
+      deptList,
+      getUserList,
+      handleQuery,
+      handleReset,
+      handleCurrentChange,
+      handleDel,
+      handlePatchDel,
+      handleSelectionChange,
+      handleCreate,
+      getDeptList,
+      getRoleList,
+      handleClose,
+      handleSubmit,
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
