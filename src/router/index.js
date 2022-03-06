0a958a3356b1536d92021fb77e489db6d7bf8ec4
qiyu <157453032@qq.com>
2022-03-06 16:33:10 +0800

    创建动态路由、导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
+import storage from '../utils/storage.js'
+import API from '../api/index.js'
+import utils from '../utils/utils.js'
 
 const routes = [
   {
@@ -15,42 +18,10 @@ const routes = [
         name: 'welcome',
         path: '/welcome',
         meta: {
-          title:'欢迎页'
+          title:'欢迎体验'
         },
         component: ()=>import('@/views/Welcome.vue')
       },
-      {
-        name: 'user',
-        path: '/system/user',
-        meta: {
-          title:'用户管理'
-        },
-        component: () => import('@/views/User.vue'),
-      },
-      {
-        name: 'menu',
-        path: '/system/menu',
-        meta: {
-          title:'菜单管理'
-        },
-        component: () => import('@/views/Menu.vue'),
-      },
-       {
-        name: 'role',
-        path: '/system/role',
-        meta: {
-          title:'角色管理'
-        },
-        component: () => import('@/views/Role.vue'),
-      },
-      {
-        name: 'dept',
-        path: '/system/dept',
-        meta: {
-          title:'部门管理'
-        },
-        component: () => import('@/views/Dept.vue')
-       }
     ]
   },
   {
@@ -60,6 +31,14 @@ const routes = [
       title:'登录'
     },
     component: ()=> import('@/views/Login.vue')
+  },
+  {
+    name: '404',
+    path: '/404',
+    meta: {
+      title:'页面不存在'
+    },
+    component: ()=> import('@/views/404.vue')
   }
 ]
 const router = createRouter({
@@ -67,4 +46,40 @@ const router = createRouter({
   routes
 })
 
+async function loadAsyncRoutes() {
+  let userInfo = storage.getItem('userInfo') || {}
+  if (userInfo.token) {
+   try{
+      const {menuList} = await API.getPermissionList()
+      let routes = utils.generateRoute(menuList)
+     routes.map(route => {
+        let url = `./../views/${route.component}.vue`
+        route.component = () => import(url)
+        router.addRoute("home", route)
+    })
+   } catch (error) {
+  }
+  }
+}
+
+
+await loadAsyncRoutes()
+// 判断当前地址是否可以访问
+ function checkPermission(path) {
+  let hasPermission = router.getRoutes().filter(route => route.path == path).length
+  if (hasPermission) {
+    return true
+  } else {
+    return false
+  }
+} 
+// 导航守卫
+ router.beforeEach((to,from,next) => {
+  if (checkPermission(to.path)) {
+    document.title = to.meta.title
+    next()
+  } else {
+    next('/404')
+    }
+}) 
 export default router
